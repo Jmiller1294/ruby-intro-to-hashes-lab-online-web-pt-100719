b0b151fd6175f3c56d6858c324261fafe24642c7
Justin Miller <jayman1294@gmail.com>
2019-10-20 08:06:39 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -72,11 +72,11 @@ def monopoly_with_fourth_tier
 	        },
 	        :b_and_o_railroad => 
 	        {
-	          "mortgage_value" => "$200"
+	          "mortgage_value" => "$400"
 	        },
 	        :shortline => 
 	        {
-	          
+	          "mortgage_value" => "$800"
 	        }
 	      }, 
 	      :rent_in_dollars => 
